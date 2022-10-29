90e2c02d6550bf0737aabdcbe5f30b644c4cd942
Kevin Berger <kevin.berger@mcs.ch>
2022-10-29 18:53:39 +0200

    Clean up

--- components/GalleryImage.js
+++ components/GalleryImage.js
@@ -2,7 +2,4 @@ import styles from '../styles/GalleryImage.module.css'
 
 export default function GalleryImage({ src, alt }) {
     return  <img className={styles.image} alt={alt} src={src} />
-    // return <article className={styles.container}>
-    //     <img className={styles.image} alt={alt} src={src} />
-    // </article>
 }
