e81945f51c826ba3864f14743712e4d90cd94f8f
Kevin Berger <kevin.berger@mcs.ch>
2022-10-29 18:22:06 +0200

    Try to style Gallery

--- components/GalleryImage.js
+++ components/GalleryImage.js
@@ -1,5 +1,8 @@
+import styles from '../styles/GalleryImage.module.css'
+
 export default function GalleryImage({ src, alt }) {
-    return <>
-        <img alt={alt} src={src} />
-    </>
+    return  <img className={styles.image} alt={alt} src={src} />
+    // return <article className={styles.container}>
+    //     <img className={styles.image} alt={alt} src={src} />
+    // </article>
 }
--- pages/gallery.js
+++ pages/gallery.js
@@ -6,9 +6,11 @@ export default function GalleryPage() {
     const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"]
 
     return <main className={styles.container}>
+        <ul>
         { images.map(image => {
-                return <GalleryImage src={"/gallery/" + image} />
+            return <li><GalleryImage src={"/gallery/" + image} /></li>
             })
         }
+        </ul>
     </main>
 }
