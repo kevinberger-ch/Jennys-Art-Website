ab82b66bac3570200263ec1956c224962841cab7
Kevin Berger <kevin.berger@mcs.ch>
2022-10-29 16:09:25 +0200

    Refactor Underlined Component and fix Implementations

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,17 +5,17 @@ export default function Navigation() {
     return <header className={styles.container}>
         <ul>
             <li>
-                <Underlined>
+                <Underlined width="120px">
                     <a className={styles.link} href="/about">About</a>
                 </Underlined>
             </li>
             <li>
-                <Underlined>
+                <Underlined width="130px">
                     <a className={styles.link} href="/gallery">Gallery</a>
                 </Underlined>
             </li>
             <li>
-                <Underlined>
+                <Underlined width="130px">
                     <a className={styles.link} href="/contact">Contact</a>
                 </Underlined>
             </li>
--- components/Underlined.js
+++ components/Underlined.js
@@ -1,7 +1,7 @@
 import styles from '../styles/Underlined.module.css'
 
-export default function Underlined({ children }) {
-    return <span className={styles.container}>
+export default function Underlined({ children, width }) {
+    return <span className={styles.container} style={{width: width}}>
         {children}
         <img className={styles.image} src="/underline.png"  alt="underlined text"/>
     </span>
