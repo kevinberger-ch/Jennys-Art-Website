37e8b939fd8947f1e477ee858bd735a1220e3f73
Kevin Berger <kevin.berger@mcs.ch>
2022-10-29 16:29:12 +0200

    Change Website Structure to Grid

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,11 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <>
+  return <div className="website">
     <Navigation />
     <Component {...pageProps} />
     <Footer />
-  </>
+  </div>
 }
 
 export default MyApp
