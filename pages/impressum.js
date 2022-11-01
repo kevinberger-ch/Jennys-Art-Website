e3bd7ed0a24398d5af12a931320e7b427398b3be
Kevin Berger <kevin.berger@mcs.ch>
2022-11-01 19:23:03 +0100

    Add Impressum Page

---
+++ pages/impressum.js
@@ -0,0 +1,9 @@
+import styles from "../styles/LandingPage.module.css";
+
+export default function ImpressumPage() {
+    return <main className={styles.container}>
+        <section className={styles.opening}>
+            <h1>Impressum</h1>
+        </section>
+    </main>
+}
