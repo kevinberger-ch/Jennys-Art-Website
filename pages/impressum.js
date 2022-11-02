6790725ab6a5f8f1c01ae65baf8e310cad90fc6b
Kevin Berger <kevin.berger@mcs.ch>
2022-11-02 18:01:25 +0100

    Add Impressum page

--- pages/impressum.js
+++ pages/impressum.js
@@ -1,9 +1,13 @@
-import styles from "../styles/LandingPage.module.css";
+import styles from "../styles/ImpressumPage.module.css";
 
 export default function ImpressumPage() {
     return <main className={styles.container}>
-        <section className={styles.opening}>
+        <section className={styles.content}>
             <h1>Impressum</h1>
+            <p>Herr Kevin Berger</p>
+            <p>Alte Strasse 55<br />
+            CH-3713 Reichenbach i. K.</p>
+            <p>mail@kevinberger.ch</p>
         </section>
     </main>
 }
