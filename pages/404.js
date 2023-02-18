3b07970b0914c919fcd0c68106e8a20d0c913401
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:29 +0100

    Add 404 Not Found Page

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import styles from '../styles/LandingPage.module.css'
+
+export default function NotFoundPage() {
+    return <main className={styles.container}>
+        <section className={styles.opening}>
+            <h1>This page could not be found.</h1>
+        </section>
+    </main>
+}
