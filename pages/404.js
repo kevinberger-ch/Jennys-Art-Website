bd2a36573292a3648f30b7fbd1f00859f3d1c639
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:29 +0100

    Update 404 Page

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,8 @@ import styles from '../styles/LandingPage.module.css'
 export default function NotFoundPage() {
     return <main className={styles.container}>
         <section className={styles.opening}>
-            <h1>This page could not be found.</h1>
+            <h1>404</h1>
+            <h2>This page could not be found.</h2>
         </section>
     </main>
 }
