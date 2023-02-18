b2433a6d841159bfbde1985059908fddf468c7f5
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:29 +0100

    Expand design

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,20 @@ import {useEffect} from "react";
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     document.title = "Jenny's Art";
+    let vh = window.innerHeight * 0.01;
+
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
   });
+
+
+
 
   return <section className="website">
     <Header />
