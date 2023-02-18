2e400d080cde84709191651ba1fbbbf075627a38
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:29 +0100

    Fix w3c Errors

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import styles from '../styles/LandingPage.module.css'
 
 export default function LandingPage() {
   return <main className={styles.container}>
-    <section className={styles.opening}>
+    <div className={styles.opening}>
       <h1>JENNY'S</h1>
       <h2>ART</h2>
-    </section>
+    </div>
   </main>
 }
