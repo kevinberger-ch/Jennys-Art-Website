c4381d4518c3b33fed782c54bbf55369b2876f76
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:28 +0100

    Expand LandingPage

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,11 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <div className="website">
+  return <section className="website">
     <Navigation />
     <Component {...pageProps} />
     <Footer />
-  </div>
+  </section>
 }
 
 export default MyApp
