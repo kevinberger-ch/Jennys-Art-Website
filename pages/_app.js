e8338f1c0da404c0c77db7001d7360cd89a704a4
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:28 +0100

    Rename Navigation to Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import '../styles/globals.css'
-import Navigation from "../components/Navigation";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return <section className="website">
-    <Navigation />
+    <Header />
     <Component {...pageProps} />
     <Footer />
   </section>
