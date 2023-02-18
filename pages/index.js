d527f082f750d8f35d098223d83b5f969efaa29e
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:28 +0100

    Change Website Structure to Grid

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import styles from '../styles/LandingPage.module.css'
 
 export default function LandingPage() {
-  return <>
-  </>
+  return <main>
+    <h1>JENNY'S</h1>
+    <h2>ART</h2>
+  </main>
 }
