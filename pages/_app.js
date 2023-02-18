a92c6cf86f429a2d040fd221b5acf1ceac90717c
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:29 +0100

    Add Impressum Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import {useEffect} from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.title = "Jenny's Art";
+  });
+
   return <section className="website">
     <Header />
     <Component {...pageProps} />
