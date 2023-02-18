bc1119a08bfc8e3e04ceeead2510f1346785c737
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:28 +0100

    Implement GalleryImage in /gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,6 +1,14 @@
 import styles from "../styles/GalleryPage.module.css";
+import GalleryImage from "../components/GalleryImage";
+
 
 export default function GalleryPage() {
+    const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"]
+
     return <main className={styles.container}>
+        { images.map(image => {
+                return <GalleryImage src={"/gallery/" + image} />
+            })
+        }
     </main>
 }
