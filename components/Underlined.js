02008c9a96637d289622449af7ff215555dce904
Kevin Berger <mail@kevinberger.ch>
2023-02-18 16:16:28 +0100

    Add Style for Navigation and Underlined

--- components/Underlined.js
+++ components/Underlined.js
@@ -1,6 +1,8 @@
+import styles from '../styles/Underlined.module.css'
+
 export default function Underlined({ children }) {
-    return <span>
+    return <span className={styles.container}>
         {children}
-        <img src="/underline.png"  alt="underlined text"/>
+        <img className={styles.image} src="/underline.png"  alt="underlined text"/>
     </span>
 }
